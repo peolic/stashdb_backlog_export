ff16bc789d6df4ef0d1bc6c3c3f1d607186c378d
peolic <66393006+peolic@users.noreply.github.com>
2022-05-28 17:31:09 +0300

    Update performers_to_split_up.py

--- extract/sheets/performers_to_split_up.py
+++ extract/sheets/performers_to_split_up.py
@@ -73,7 +73,7 @@ class PerformersToSplitUp(BacklogBase):
 
         return self.RowResult(row.num, done, item)
 
-    LABELS_PATTERN = re.compile(r'\[?(stashdb|iafd|i(?:nde)?xxx|thenude|twitter|gevi)\]?', re.I)
+    LABELS_PATTERN = re.compile(r'\[?(stash(?:db)?|iafd|i(?:nde)?xxx|thenude|twitter|gevi)\]?', re.I)
 
     def _get_shards(self, cells: List[SheetCell], row_num: int) -> List[SplitShard]:
         results: List[SplitShard] = []
